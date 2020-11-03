10e8513e8ffa5ddaf32fcde5b15f73272028c739
yeseuloh <38956218+yeseuloh@users.noreply.github.com>
2020-11-02 23:35:47 -0400

    created general to do list format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
+  const [list, setList] = React.useState([]);
+
+  let todoInput = React.createRef();
+
+  const updateList = (e) => {
+    e.preventDefault();
+    setList(list => [...list, todoInput]);
+  };
+  console.log(list);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-wrapper">
+      <h1>To Do List</h1>
+      {list.length > 0 && (
+        <div className="todo-output-wrapper">
+          {list.map((todo, index) => (
+            <div key={index}>{todo.toString()}</div>
+          ))}
+        </div>
+      )}
+      <form className="todo-input-wrapper">
+        <input type="text" ref={todoInput} />
+        <button className="button-update" onClick={updateList}>
+          Submit
+        </button>
+        <button className="button-delete" >
+          Delete
+        </button>
+      </form>
     </div>
   );
 }
