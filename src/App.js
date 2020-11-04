d5b467e4e52acd587988cbb258e5da052648607e
yeseuloh <38956218+yeseuloh@users.noreply.github.com>
2020-11-03 21:26:22 -0400

    added completed prop

--- src/App.js
+++ src/App.js
@@ -4,13 +4,34 @@ import "./App.css";
 
 function App() {
   const [list, setList] = React.useState([]);
+  // const [todoInput, setTodoInput] = React.useState("");
+  const [todoInput, setTodoInput] = React.useState({
+    title: "",
+    completed: false
+  });
 
-  let todoInput = React.createRef();
-
-  const updateList = (e) => {
-    e.preventDefault();
-    setList(list => [...list, todoInput.current.value]);
+  // let todoInput = React.createRef();
+  const updateTodoInput = e => {
+    setTodoInput({ title: e.target.value, completed: false });
   };
+
+  const updateList = e => {
+    e.preventDefault();
+    setList(list => [...list, todoInput]);
+    setTodoInput({ title: "", completed: false });
+  };
+
+  const markItem = (e, index) => {
+    e.preventDefault();
+    // var stateCopy = Object.assign({}, list);
+    // if (stateCopy[index].completed) {
+    //   stateCopy[index].completed = false;
+    // } else {
+    //   stateCopy[index].completed = true;
+    // }
+    // setList(stateCopy);
+  };
+
   console.log(list);
 
   return (
@@ -19,17 +40,31 @@ function App() {
       {list.length > 0 && (
         <div className="todo-output-wrapper">
           {list.map((todo, index) => (
-            <div key={index}>{todo.toString()}</div>
+            <>
+              {!todo.completed && (
+                <div className="todo-output-item">
+                  <p key={index}>{todo.title.toString()}</p>
+                  <button className="button-delete" onClick={ e => markItem(e, index)}>
+                    Done
+                  </button>
+                </div>
+              )}
+              {todo.completed && (
+                <div className="todo-output-item">
+                <p key={index} style={{textDecoration: 'line-through'}}>{todo.title.toString()}</p>
+                <button className="button-delete" onClick={markItem(todo)}>
+                  Uncheck
+                </button>
+              </div>
+              )}
+            </>
           ))}
         </div>
       )}
       <form className="todo-input-wrapper">
-        <input type="text" ref={todoInput}/>
+        <input type="text" onChange={updateTodoInput} value={todoInput.title} />
         <button className="button-update" onClick={updateList}>
           Submit
-        </button>
-        <button className="button-delete" >
-          Delete
         </button>
       </form>
     </div>
